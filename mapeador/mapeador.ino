e85db37e1edfb4a78069878d78c382a52ee85056
Vitor Rasp <vtrshaft@gmail.com>
2020-11-19 14:11:32 -0300

    inserida rotação do radar do mapeador. Obs detectados retornam distancia 0

--- mapeador/mapeador.ino
+++ mapeador/mapeador.ino
@@ -3,7 +3,7 @@
  *   Sketch para Arduino em plataforma robótica com:
  *   - Motores DC com encoders (20 furos)
  *   - Sensor de distância ultrassom HC-SR04
- *   - Módulo BLuetooth HC-05
+ *   - Módulo Bluetooth HC-05
  *   - Ponte-H L9110
  */
 
@@ -13,8 +13,17 @@
   *  - Frente, trás, esquerda, direita
   *  - Odometria
   */
-#define trig 7
-#define echo 8
+#include <Servo.h>
+Servo sonar;
+const int trig = 7;
+const int echo = 8;
+int cont = 0;
+int d;
+
+struct Radar{
+  int az;
+  int d;
+};
 
 int medirDist(){
   long duracao;
@@ -31,20 +40,59 @@ int medirDist(){
   // Calculating the distance
   dist= duracao*0.034/2;
   // Prints the distance on the Serial Monitor
-  //Serial.print("Distancia: ");
-  //Serial.println(dist);
-  return(dist);
-  
+  if(dist > 100){
+    dist = 100;
+  }
+  return dist;
+}
+
+void girarSonar(){
+  //Radar radar;
+  int azim = 0;
+  int d;
+  for(azim = 0; azim <= 180; azim+=10){
+    sonar.write(azim);
+    delay(30);
+    d = medirDist();
+    delay(500);
+    Serial.print("Angulo: ");
+    Serial.println(azim);
+    Serial.print("Distancia: ");
+    Serial.println(d);
+  }
+  /*
+  for(azim = 180; azim >= 0; azim-=1){
+    sonar.write(azim);
+    cp[0] = medirDist();
+    cp[1] = azim;
+    delay(20);
+  }
+  */
 }
 
 void setup() {
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
+  sonar.attach(5);
   Serial.begin(9600);
+  
 
 }
 
 void loop() {
-  Serial.println(medirDist());
-
+  //Serial.println(medirDist());
+  //medirDist();
+  
+  //int azim = 0;
+  
+  for(int azim = 0; azim <= 180; azim+=10){
+    sonar.write(azim);
+    delay(30);
+    d = medirDist();
+    delay(500);
+    Serial.print("Angulo: ");
+    Serial.println(azim);
+    Serial.print("Distancia: ");
+    Serial.println(d);
+  }
 }

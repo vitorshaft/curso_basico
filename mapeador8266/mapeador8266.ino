40bd2d3992e42a942a874cf3ea5de3e623c95b8b
vitorshaft <vtrshaft@gmail.com>
2020-12-11 13:51:17 -0300

    inserido webserver - mapeador8266

--- mapeador8266/mapeador8266.ino
+++ mapeador8266/mapeador8266.ino
@@ -1,3 +1,24 @@
+//copiado do exemplo HelloServer na cara dura
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#ifndef STASSID
+#define STASSID "SHAFT"
+#define STAPSK  "recife2014"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp8266!");
+}
+
 const int encEsq = 12;
 const int encDir = 14;
 
@@ -12,12 +33,36 @@ void dir(){
 }
 
 void setup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
   attachInterrupt(digitalPinToInterrupt(encEsq),esq,CHANGE);
   attachInterrupt(digitalPinToInterrupt(encDir),dir,CHANGE);
 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  //INSERIR BROADCAST DO ENCODER VIA WIFI
+  server.handleClient();
+  MDNS.update();
 
 }
